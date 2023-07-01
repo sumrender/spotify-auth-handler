bd4811c6b6860c457caf36346c442b36384ea937
Sumrender <sumrenders@gmail.com>
2023-07-01 18:16:32 +0530

    v1

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+const clientId = process.env.CLIENT_ID;
+const redirectUri = process.env.REDIRECT_URI;
+
+app.get("/", (req, res) => {
+  res.send("server is running.");
+});
+
+app.get("/auth", (req, res) => {
+  const scopes = [
+    "playlist-read-private",
+    "playlist-modify-public",
+    "playlist-modify-public",
+  ];
+
+  const scopeString = scopes.join(" ");
+
+  const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+    redirectUri
+  )}&scope=${encodeURIComponent(scopeString)}&response_type=token`;
+
+  res.redirect(url);
+});
+
+app.get("/callback", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+const PORT = 5173;
+app.listen(PORT, console.log(`Server listening on port ${PORT}`));
